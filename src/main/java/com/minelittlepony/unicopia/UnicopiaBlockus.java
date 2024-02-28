6c719eaa972a11fa4b2a2b471277e39b74c52183
Sollace <sollacea@gmail.com>
2024-02-28 15:00:12 +0000

    Project scaffolding

---
+++ src/main/java/com/minelittlepony/unicopia/UnicopiaBlockus.java
@@ -0,0 +1,17 @@
+package com.sollace.unicopia;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UnicopiaBlockus implements ModInitializer {
+    public static final String DEFAULT_NAMESPACE = "unicopia";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    public void onInitialize() {
+    }
+}
